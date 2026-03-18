d2b1f82f4549e5fb2fed5f14be54217c10d00ec4
Dhanya Venkatesh <159805101+its-dhanya@users.noreply.github.com>
2026-03-18 10:49:21 +0530

    remove confidential info

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 // ─── STEP 1: Paste your Firebase config here ───────────────
 // Firebase Console → Project Settings → Your apps → Web app → SDK setup
 const firebaseConfig = {
-    apiKey: "AIzaSyBDeq6g3g7NYyCTo1AFUmNQ2xYsJOPYYwE",
-    authDomain: "smartcampusconnect-bf148.firebaseapp.com",
-    projectId: "smartcampusconnect-bf148",
-    storageBucket: "smartcampusconnect-bf148.firebasestorage.app",
-    messagingSenderId: "777527724322",
-    appId: "1:777527724322:web:43ceb34e12f4eaf36ffd7b"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   
 // ───────────────────────────────────────────────────────────
@@ -29,7 +29,7 @@ export const requestFCMToken = async () => {
     // Firebase Console → Project Settings → Cloud Messaging
     // → Web Push certificates → Generate key pair → copy Key pair
     const token = await getToken(messaging, {
-      vapidKey: 'BIU6-GJUmjjYIvS5jPUZqVGI_pHEn_01X0_3gBDdfXz1FjIy30Lvbd7oicjAdEddhrvUbS38js84EhuzD7rZLrQ',
+      vapidKey: '',
     });
     // ───────────────────────────────────────────────────────
 
